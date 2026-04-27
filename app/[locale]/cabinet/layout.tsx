3f21b30521a0cce395311e266c408884b16f73be
muhammadrizobaxtiyorov9-gif <muhammadrizobaxtiyorov9@gmail.com>
2026-04-27 20:46:47 +0500

    fixed

--- app/[locale]/cabinet/layout.tsx
+++ app/[locale]/cabinet/layout.tsx
@@ -4,16 +4,13 @@ import CabinetLayoutClient from './CabinetLayoutClient';
 export default async function CabinetLayout({
   children,
   params,
-  searchParams,
 }: {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const { locale } = await params;
-  const sp = await searchParams;
   // This will redirect to login if not authenticated
-  const client = await getAuthenticatedClient(locale, sp);
+  const client = await getAuthenticatedClient(locale);
 
   return (
     <CabinetLayoutClient clientId={client.id} clientName={client.name}>
